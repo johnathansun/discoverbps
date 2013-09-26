c01bc2d2cee828b394f43d6956b82062aa02ced2
Joel Mahoney <joelmahoney@gmail.com>
2013-09-25 18:01:27 -0700

    Initial setup and layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscoverbpsV2::Application.config.secret_token = 'ffd00bbb92254d9327b5fc7cff089d516727654070ca9e63c513dffb1ef30e61570edbd2b3bcbdef477908875f7e481d97aaf167563118f907e5f945b3c2318b'
