9fb9969b154b41e8212a77144ef022bdfad61abc
Joel Mahoney <joelmahoney@gmail.com>
2013-09-26 18:23:04 -0700

    Hide attr_accessible

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,14 +1,18 @@
 class School < ActiveRecord::Base
-  # attr_accessible :title, :body
-    serialize :api_basic_info     
-		serialize :api_awards
-		serialize :api_calendar
-		serialize :api_description
-		serialize :api_extra_curricular
-		serialize :api_facilities
-		serialize :api_grades
-		serialize :api_hours
-		serialize :api_languages
-		serialize :api_partners
-		serialize :api_photos
+	extend FriendlyId
+	friendly_id :name, use: :slugged
+
+	# attr_accessible :name, :bps_id, :api_basic_info, :api_awards, :api_calendar, :api_description, :api_extra_curricular, :api_facilities, :api_grades, :api_hours, :api_languages, :api_partners, :api_photos
+
+	serialize :api_basic_info     
+	serialize :api_awards
+	serialize :api_calendar
+	serialize :api_description
+	serialize :api_extra_curricular
+	serialize :api_facilities
+	serialize :api_grades
+	serialize :api_hours
+	serialize :api_languages
+	serialize :api_partners
+	serialize :api_photos
 end
