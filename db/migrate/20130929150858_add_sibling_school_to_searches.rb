11d452d464306a1e84771e58541366b2b990d270
Joel Mahoney <joelmahoney@gmail.com>
2013-09-29 20:58:48 -0700

    Remove search migrations

--- db/migrate/20130929150858_add_sibling_school_to_searches.rb
+++
@@ -1,6 +0,0 @@
-class AddSiblingSchoolToSearches < ActiveRecord::Migration
-  def change
-  	add_column :searches, :sibling_school_name, :string
-  	add_column :searches, :sibling_school_id, :integer
-  end
-end
