a4e89fd261046eca0ca87a591df8d2d2cf4aca49
Joel Mahoney <joelmahoney@gmail.com>
2013-10-01 02:12:12 -0700

    Disable store location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_student
-  after_filter :store_location
+  # after_filter :store_location
 
   private
   
@@ -11,22 +11,22 @@ class ApplicationController < ActionController::Base
 	  end
   end
 
-	def store_location
-	 # store last url - this is needed for post-login redirect to whatever the user last visited.
-	    if (request.fullpath != "/users/sign_in" && \
-	        request.fullpath != "/users/sign_up" && \
-	        request.fullpath != "/users/password" && \
-	        !request.xhr?) # don't store ajax calls
-	      session[:previous_url] = request.fullpath 
-	    end
-	end
+	# def store_location
+	#  # store last url - this is needed for post-login redirect to whatever the user last visited.
+	#     if (request.fullpath != "/users/sign_in" && \
+	#         request.fullpath != "/users/sign_up" && \
+	#         request.fullpath != "/users/password" && \
+	#         !request.xhr?) # don't store ajax calls
+	#       session[:previous_url] = request.fullpath 
+	#     end
+	# end
 
 	def after_sign_out_path_for(resource_or_scope)
 	  root_url
 	end
 
 	def after_sign_up_path_for(resource)
-	  session[:previous_url] || root_path
+	  root_path
 	end
 
 	def after_sign_in_path_for(resource)
