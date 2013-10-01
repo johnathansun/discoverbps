b3792fce399435490bf40b9a1f6c9e1400b8b614
Joel Mahoney <joelmahoney@gmail.com>
2013-10-01 02:35:21 -0700

    Add registrations route to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 DiscoverbpsV2::Application.routes.draw do
-  devise_for :users, :controllers => { :sessions => "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :sessions => "users/sessions", :registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root :to => 'schools#home'
 
