87f9e564b8e02897ec5d0a044412d7369577c1d6
Joel Mahoney <joelmahoney@gmail.com>
2013-10-24 16:38:16 -0700

    Make student tabs hyperspan buttons; calculate today timeline percentage; fix dialog box close buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def timeline_percent(today)
+		if today >= Date.new(today.year, 10, 01)
+			"#{ ((((today - today.beginning_of_year) - 272) / 365.0).to_f * 100).round }%"
+		else
+			"#{ (((today - today.beginning_of_year) / 365.0).to_f * 100).round }%"
+		end
+	end
 end
