929576068582598a3864b8e07b5e448c3951de34
Joel Mahoney <joelmahoney@gmail.com>
2013-10-28 14:27:24 -0700

    Limit street_number length to 5

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
   acts_as_paranoid
-  
+
   belongs_to :user
 	has_and_belongs_to_many :preferences, uniq: true
 	has_many :student_schools, uniq: true
@@ -9,6 +9,7 @@ class Student < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :grade_level, :iep, :primary_language, :session_id, :sibling_school_id, :sibling_school_name, :street_name, :street_number, :neighborhood, :zipcode, :latitude, :longitude, :user_id, :preference_ids, :school_ids, :iep_needs, :ell_needs, :schools_last_updated_at, :x_coordinate, :y_coordinate
   
   validates :street_number, :street_name, :zipcode, :grade_level, presence: true
+  validates :street_number, length: { maximum: 5 }
   validates :grade_level, inclusion: { in: %w(K0 K1 K2 1 2 3 4 5 6 7 8 9 10 11 12),
     message: "%{value} is not valid" }
 
