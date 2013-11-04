babf3c8e71c68531d8d98d991f345fdf82cc69ea
Joel Mahoney <joelmahoney@gmail.com>
2013-11-03 19:08:18 -0800

    Remove coming soon route

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ DiscoverbpsV2::Application.routes.draw do
   resources :schools do 
     get 'home', on: :collection
     get 'zone_schools', on: :collection
-    get 'coming_soon', on: :collection
     get 'print', on: :collection
     post 'sort', on: :collection
     get 'compare', on: :collection
