8cbdaf3bde5b0a5fa5e192a0038b808f21034ea5
Joel Mahoney <joelmahoney@gmail.com>
2013-11-05 14:41:50 -0800

    Change find_for_oauth method name to find_or_create_from_oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def facebook
-    @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_or_create_from_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
@@ -12,7 +12,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
 	def twitter
-    @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_or_create_from_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
     	puts "********************** user is persisted"
