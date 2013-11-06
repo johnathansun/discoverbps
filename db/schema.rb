34e98b5b80326fccd4ca16e12f247a2a397a6568
Joel Mahoney <joelmahoney@gmail.com>
2013-11-05 16:03:46 -0800

    Remove null false requirement on user email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131030212719) do
+ActiveRecord::Schema.define(:version => 20131106000253) do
 
   create_table "preference_categories", :force => true do |t|
     t.string   "name"
@@ -179,7 +179,7 @@ ActiveRecord::Schema.define(:version => 20131030212719) do
   add_index "students", ["user_id"], :name => "index_students_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
+    t.string   "email",                  :default => ""
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
