1cab3e9c9ac9979d1d3427b2aa46706746b50063
Joel Mahoney <joelmahoney@gmail.com>
2013-12-15 03:07:09 -0800

    Sign in and sign up via ajax requests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,26 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-	layout 'home'
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        # set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        logger.info "****************** resource active_for_authentication?"
+        return render :json => {:success => true}
+      else
+        # set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        logger.info "****************** resource not active_for_authentication"
+	      return render :json => {:success => true}
+      end
+    else
+      clean_up_passwords resource
+      logger.info "****************** resource not saved"
+	    return render :json => {:success => false, :errors => ["Signup failed."]}
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
 
+	##### Devise mappings, from http://natashatherobot.com/devise-rails-sign-in/
+	
+	def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+	#####
+
 	def timeline_percent(today)
 		if today >= Date.new(today.year, 10, 01)
 			"#{ ((((today - today.beginning_of_year) - 272) / 365.0).to_f * 100).round }%"
