171f4aa86ad1d04b9d7a85124a6bc9ee6d039e44
Joel Mahoney <joelmahoney@gmail.com>
2014-02-03 19:27:58 -0800

    Ensure that values returned from the API are strings (so they can be compared with the values entered through the admin interface)

--- app/models/school.rb
+++ app/models/school.rb
@@ -45,7 +45,7 @@ class School < ActiveRecord::Base
 		if table.blank? || key.blank? || value.blank?
 			return false
 		else
-			if self.send(table).present? && self.send(table).try(:[], key.to_sym).present? && self.send(table).try(:[], key.to_sym) == value
+			if self.send(table).present? && self.send(table).try(:[], key.to_sym).present? && self.send(table).try(:[], key.to_sym).try(:to_s) == value
 				return true
 			else
 				return false
