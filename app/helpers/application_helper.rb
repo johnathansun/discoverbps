8a12cab898a62bd22ae94c336e8816f94c0bf50e
Joel Mahoney <joelmahoney@gmail.com>
2014-02-04 10:27:49 -0800

    Fix timeline today location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,9 +18,9 @@ module ApplicationHelper
 
 	def timeline_percent(today)
 		if today >= Date.new(today.year, 10, 01)
-			"#{ ((((today - today.beginning_of_year) - 272) / 365.0).to_f * 100).round }%"
+			"#{ ((((today - today.beginning_of_year).to_f - 273) / 365) * 100).round }%"
 		else
-			"#{ (((today - today.beginning_of_year) / 365.0).to_f * 100).round }%"
+			"#{ ((((today - today.beginning_of_year).to_f + 93) / 365) * 100).round }%"
 		end
 	end
 end
