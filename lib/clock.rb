5140f64b8ba55d0219389a94b855fe31fb9ecd33
Joel Mahoney <joelmahoney@gmail.com>
2014-02-23 10:47:27 -0800

    Work on clock file

--- lib/clock.rb
+++ lib/clock.rb
@@ -9,13 +9,13 @@ handler do |job|
 end
 
 # 9 UTC = 3am EST and 12am PST
-every(1.day, at: '9:00', 'Update school basic info') 		{ SchoolData.delay.update_basic_info! }
-every(1.day, at: '9:00', 'Update school awards') 				{ SchoolData.delay.update_awards! }
-every(1.day, at: '9:00', 'Update school descriptions') 	{ SchoolData.delay.update_descriptions! }
-every(1.day, at: '9:00', 'Update school facilities') 		{ SchoolData.delay.update_facilities! }
-every(1.day, at: '9:00', 'Update school grades') 				{ SchoolData.delay.update_grades! }
-every(1.day, at: '9:00', 'Update school hours') 				{ SchoolData.delay.update_hours! }
-every(1.day, at: '9:00', 'Update school languages') 		{ SchoolData.delay.update_languages! }
-every(1.day, at: '9:00', 'Update school partners') 			{ SchoolData.delay.update_partners! }
-every(1.day, at: '9:00', 'Update school photos') 				{ SchoolData.delay.update_photos! }
-every(1.day, at: '9:00', 'Update school sports') 				{ SchoolData.delay.update_sports! }	
+every(1.day, 'Update school basic info', :at => '20:00') { SchoolData.delay.update_basic_info! }
+every(1.day, 'Update school awards', :at => '20:00') { SchoolData.delay.update_awards! }
+every(1.day, 'Update school descriptions', :at => '20:00') { SchoolData.delay.update_descriptions! }
+every(1.day, 'Update school facilities', :at => '20:00') { SchoolData.delay.update_facilities! }
+every(1.day, 'Update school grades', :at => '20:00') { SchoolData.delay.update_grades! }
+every(1.day, 'Update school hours', :at => '20:00') { SchoolData.delay.update_hours! }
+every(1.day, 'Update school languages', :at => '20:00') { SchoolData.delay.update_languages! }
+every(1.day, 'Update school partners', :at => '20:00') { SchoolData.delay.update_partners! }
+every(1.day, 'Update school photos', :at => '20:00') { SchoolData.delay.update_photos! }
+every(1.day, 'Update school sports', :at => '20:00') { SchoolData.delay.update_sports! }	
