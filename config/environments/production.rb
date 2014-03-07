090adad46de6811e552c4ef1ec3b19d5e3701500
Joel Mahoney <joelmahoney@gmail.com>
2014-03-06 17:41:16 -0800

    Remove CDN config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ DiscoverbpsV2::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = ENV['CDN_SUMO_URL']
-  config.static_cache_control = "public, max-age=2592000"
+  # config.action_controller.asset_host = ENV['CDN_SUMO_URL']
+  # config.static_cache_control = "public, max-age=2592000"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
