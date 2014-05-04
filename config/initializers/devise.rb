aed9914548493d7318081935c221d15b6868a08e
Joel Mahoney <joelmahoney@gmail.com>
2014-05-04 09:41:11 -0700

    More work on password recovery flow

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -188,6 +188,8 @@ Devise.setup do |config|
   # change their passwords.
   config.reset_password_within = 6.hours
 
+  config.allow_insecure_token_lookup = true
+
   # ==> Configuration for :encryptable
   # Allow you to use another encryption algorithm besides bcrypt (default). You can use
   # :sha1, :sha512 or encryptors from others authentication tools as :clearance_sha1,
