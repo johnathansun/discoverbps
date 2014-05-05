3458a92b8880c015749bfe618163fd675f1a24b4
Joel Mahoney <joelmahoney@gmail.com>
2014-05-04 20:21:47 -0700

    Fix missing template path; adjust modal top margin

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,16 +13,16 @@ class Users::SessionsController < Devise::SessionsController
 
 	# POST /resource/sign_in
 	def create
-			self.resource = warden.authenticate!(auth_options)
-			set_flash_message(:notice, :signed_in) if is_flashing_format?
-			sign_in(resource_name, resource)
-			yield resource if block_given?
-			
-			if request.xhr?
-				render :json => {:success => true}
-			else
-				respond_with resource, location: after_sign_in_path_for(resource)
-			end
+		self.resource = warden.authenticate!(auth_options)
+		set_flash_message(:notice, :signed_in) if is_flashing_format?
+		sign_in(resource_name, resource)
+		yield resource if block_given?
+		
+		if request.xhr?
+			render :json => {:success => true}
+		else
+			respond_with resource, location: after_sign_in_path_for(resource)
+		end
 	end
 
 	# DELETE /resource/sign_out
