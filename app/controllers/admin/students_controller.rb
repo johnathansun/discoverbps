b13458f584df1ae6baaf6b81c347364cb5f2e0ec
Joel Mahoney <joelmahoney@gmail.com>
2014-07-27 09:47:12 -0700

    More work on caching json

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -8,6 +8,7 @@ class Admin::StudentsController < ApplicationController
 		respond_to do |format|
       format.html
       format.json do
+      	Rails.cache.write("searches", Student.order(:last_name).to_json(only: [ :grade_level, :latitude, :longitude, :zipcode, :ell_needs, :iep_needs, :preferences_count  ], methods: :created_at_date))
       	logger.info Rails.cache.read("searches")
 				render json: Rails.cache.read("searches")
       end
