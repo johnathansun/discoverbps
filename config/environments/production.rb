5b0d1c91d4530c8a1e3d8e7f97007e69100d49da
Joel Mahoney <joelmahoney@gmail.com>
2014-07-28 14:53:19 -0700

    More work on caching config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,28 @@
 DiscoverbpsV2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
+
+  client = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                             :username => ENV["MEMCACHIER_USERNAME"],
+                             :password => ENV["MEMCACHIER_PASSWORD"],
+                             :failover => true,
+                             :socket_timeout => 1.5,
+                             :socket_failure_delay => 0.2,
+                             :value_max_bytes => 10485760)
+  config.action_dispatch.rack_cache = {
+    :metastore    => client,
+    :entitystore  => client
+  }
+  config.static_cache_control = "public, max-age=2592000"
+
   # client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"],
   #                            :username => ENV["MEMCACHIER_USERNAME"],
   #                            :password => ENV["MEMCACHIER_PASSWORD"],
