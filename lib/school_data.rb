c07dc945043fb7ff1a3b4d575058f10772926dc0
Joel Mahoney <joelmahoney@gmail.com>
2014-08-18 01:16:36 -0700

    More work refactoring SchoolData importer

--- lib/school_data.rb
+++ lib/school_data.rb
@@ -1,272 +1,232 @@
 module SchoolData
 
-	def self.update_basic_info!(school_id=nil)
-		school_endpoint = "GetSchool?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL BASIC INFO #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_basic_info!(school_id=nil)
+		endpoint = "GetSchool"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{school_endpoint}&sch=#{school.bps_id}",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}")
 
 			if api_response.present?
 				# Use .first to store as a hash instead of a hash inside an array
       	# Alternatively, you can store as an array, but then you have to call
       	# api_basic_info.first in all partials that need to access hash values
-				hash = MultiJson.load(api_response, :symbolize_keys => true).first
-
-				school.update_attributes(name: hash[:schname_23], latitude: hash[:Latitude], longitude: hash[:Longitude], api_basic_info: hash)
-				puts "********** done updating basic info for school #{school.bps_id}"
+				begin
+					hash = MultiJson.load(api_response, :symbolize_keys => true).first
+					school.update_attributes(name: hash[:schname_23], latitude: hash[:Latitude], longitude: hash[:Longitude], api_basic_info: hash)
+				rescue
+					puts "********** Something went wrong with GetSchool id #{school.bps_id}"
+				else
+					puts "********** Updated Basic Info for id #{school.bps_id}"					
+				end
 			else
-				puts "********** API response not present for GetSchool  with id #{school.bps_id}"
+				puts "********** API response not present for GetSchool with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_awards!(school_id=nil)
-		awards_endpoint = "GetSchoolAwards?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL AWARDS #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_awards!(school_id=nil)
+		endpoint = "GetSchoolAwards"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{awards_endpoint}&sch=#{school.bps_id}&TranslationLanguage=",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}&TranslationLanguage=")
 
 			if api_response.present?
-				array = MultiJson.load(api_response, :symbolize_keys => true)
-
-				school.update_attributes(api_awards: array)
-				puts "********** done updating awards for school #{school.bps_id}"
+				SchoolData.extract_array(school, api_response, :api_awards, endpoint)
 			else
 				puts "********** API response not present for GetSchoolAwards with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_descriptions!(school_id=nil)
-  	desc_endpoint = "GetSchoolDescriptions?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL DESCRIPTIONS #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_descriptions!(school_id=nil)
+  	endpoint = "GetSchoolDescriptions"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{desc_endpoint}&sch=#{school.bps_id}&TranslationLanguage=",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}&TranslationLanguage=")
 
 			if api_response.present?
-				# Use .first to store as a hash instead of a hash inside an array
-      	# Alternatively, you can store as an array, but then you have to call
-      	# api_description.first in all partials that need to access hash values
-				hash = MultiJson.load(api_response, :symbolize_keys => true).first
-
-				school.update_attributes(api_description: hash)
-				puts "********** done updating descriptions for school #{school.bps_id}"
+				SchoolData.extract_hash(school, api_response, :api_description, endpoint)
 			else
 				puts "********** API response not present for GetSchoolDescriptions with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_facilities!(school_id=nil)
-		facilities_endpoint = "GetSchoolFacilities?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL FACILITIES #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_facilities!(school_id=nil)
+		endpoint = "GetSchoolFacilities"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{facilities_endpoint}&sch=#{school.bps_id}",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}")
 
 			if api_response.present?
-				# Use .first to store as a hash instead of a hash inside an array
-      	# Alternatively, you can store as an array, but then you have to call
-      	# api_facilities.first in all partials that need to access hash values
-				hash = MultiJson.load(api_response, :symbolize_keys => true).first
-
-				school.update_attributes(api_facilities: hash)
-				puts "********** done updating facilities for school #{school.bps_id}"
+				SchoolData.extract_hash(school, api_response, :api_facilities, endpoint)
 			else
 				puts "********** API response not present for GetSchoolFacilities with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_grades!(school_id=nil)
-		grades_endpoint = "GetSchoolGrades?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL GRADES #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_grades!(school_id=nil)
+		endpoint = "GetSchoolGrades"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			unless school.bps_id == "4670"
-				# There's something wrong with this API request when id = 4670
-				# The only thing unique about this school that I could find
-				# is that it does not have an entry for <rc></rc>. This should
-				# be reported to the API developers.
-				api_response = Faraday.new(
-					:url => "#{BPS_API_URL}#{grades_endpoint}&sch=#{school.bps_id}",
-					:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}")
 
-				if api_response.present?
-					array = MultiJson.load(api_response, :symbolize_keys => true)
-
-					school.update_attributes(api_grades: array)
-					puts "********** done updating grades for school #{school.bps_id}"
-				else
-					puts "********** API response not present for GetSchoolGrades with id #{school.bps_id}"
-				end
+			if api_response.present?
+				SchoolData.extract_array(school, api_response, :api_grades, endpoint)
+			else
+				puts "********** API response not present for GetSchoolGrades with id #{school.bps_id}"
 			end
 		end
+
 	end
 
-	def self.update_hours!(school_id=nil)
-		hours_endpoint  = "GetSchoolHours?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL HOURS #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_hours!(school_id=nil)
+		endpoint  = "GetSchoolHours"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{hours_endpoint}&sch=#{school.bps_id}&TranslationLanguage=",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}&TranslationLanguage=")
+			
 			if api_response.present?
-				# Use .first to store as a hash instead of a hash inside an array
-      	# Alternatively, you can store as an array, but then you have to call
-      	# api_facilities.first in all partials that need to access hash values
-				hash = MultiJson.load(api_response, :symbolize_keys => true).first
-
-				school.update_attributes(api_hours: hash)
-				puts "********** done updating hours for school #{school.bps_id}"
+				SchoolData.extract_hash(school, api_response, :api_hours, endpoint)
 			else
 				puts "********** API response not present for GetSchoolHours with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_languages!(school_id=nil)
-		languages_endpoint = "GetSchoolLanguages?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL LANGUAGES #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_languages!(school_id=nil)
+		endpoint = "GetSchoolLanguages"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{languages_endpoint}&sch=#{school.bps_id}",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}")
 
 			if api_response.present?
-				array = MultiJson.load(api_response, :symbolize_keys => true)
-
-				school.update_attributes(api_languages: array)
-				puts "********** done updating languages for school #{school.bps_id}"
+				SchoolData.extract_array(school, api_response, :api_languages, endpoint)
 			else
 				puts "********** API response not present for GetSchoolLanguages with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_partners!(school_id=nil)
-		partners_endpoint = "GetSchoolPartners?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL PARTNERS #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_partners!(school_id=nil)
+		endpoint = "GetSchoolPartners"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{partners_endpoint}&sch=#{school.bps_id}&TranslationLanguage=",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}&TranslationLanguage=")
 
 			if api_response.present?
-				array = MultiJson.load(api_response, :symbolize_keys => true)
-
-				school.update_attributes(api_partners: array)
-				puts "********** done updating partners for school #{school.bps_id}"
+				SchoolData.extract_array(school, api_response, :api_partners, endpoint)
 			else
 				puts "********** API response not present for GetSchoolPartners with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_photos!(school_id=nil)
-		photos_endpoint = "GetSchoolPhotos?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL PHOTOS #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_photos!(school_id=nil)
+		endpoint = "GetSchoolPhotos"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{photos_endpoint}&sch=#{school.bps_id}",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}")
 
 			if api_response.present?
-				array = MultiJson.load(api_response, :symbolize_keys => true)
-
-				school.update_attributes(api_photos: array)
-				puts "********** done updating photos for school #{school.bps_id}"
+				SchoolData.extract_array(school, api_response, :api_photos, endpoint)
 			else
 				puts "********** API response not present for GetSchoolPhotos with id #{school.bps_id}"
 			end
 		end
 	end
 
-	def self.update_sports!(school_id=nil)
-		sports_endpoint = "GetSchoolSports?schyear=#{SCHOOL_YEAR}"
+	##### UPDATE SCHOOL SPORTS #####
 
-		if school_id.present?
-			schools = School.where(id: school_id)
-		else
-			schools = School.all
-		end
+	def self.update_sports!(school_id=nil)
+		endpoint = "GetSchoolSports"
+
+		schools = SchoolData.schools_with_or_without_id(school_id)
 
 		schools.each do |school|
-			api_response = Faraday.new(
-				:url => "#{BPS_API_URL}#{sports_endpoint}&sch=#{school.bps_id}",
-				:ssl => {:version => :SSLv3}).get.body
+			api_response = SchoolData.connect_to_service("#{BPS_API_URL}/#{endpoint}?schyear=#{SCHOOL_YEAR}&sch=#{school.bps_id}")
 
 			if api_response.present?
-				# Use .first to store as a hash instead of a hash inside an array
-      	# Alternatively, you can store as an array, but then you have to call
-      	# api_facilities.first in all partials that need to access hash values
-				hash = MultiJson.load(api_response, :symbolize_keys => true).first
-
-				school.update_attributes(api_sports: hash)
-				puts "********** done updating sports for school #{school.bps_id}"
+				SchoolData.extract_hash(school, api_response, :api_sports, endpoint)
 			else
 				puts "********** API response not present for GetSchoolSports with id #{school.bps_id}"
 			end
+		end
+	end
+
+
+	private
+
+	def self.schools_with_or_without_id(school_id)
+		if school_id.present?
+			School.where(id: school_id)
+		else
+			School.all
+		end
+	end
+
+	def self.connect_to_service(url)
+		Faraday.new(:url => "#{url}", :ssl => {:version => :SSLv3}).get.body
+	end
+
+	def self.extract_hash(school, api_response, key, endpoint)
+		# Use .first to store as a hash instead of an array with a hash as the single object.
+		# The object could be stored as an array, but .first would then need to be called in all of the view partials that access this data.
+  	begin
+			hash = MultiJson.load(api_response, :symbolize_keys => true).first
+			school.update_attributes(key => hash)
+		rescue
+			puts "********** Something went wrong with #{endpoint} id #{school.bps_id}"
+		else
+			puts "********** Updated from #{endpoint} for id #{school.bps_id}"				
+		end		
+	end
+
+	def self.extract_array(school, api_response, key, endpoint)
+		begin
+			array = MultiJson.load(api_response, :symbolize_keys => true)
+			school.update_attributes(key => array)
+		rescue
+			puts "********** Something went wrong with #{endpoint} id #{school.bps_id}"
+		else
+			puts "********** Updated from #{endpoint} for id #{school.bps_id}"		
 		end
 	end
 end
