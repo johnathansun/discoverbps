e24f2f2e509eac1af672714ec5b3642bdd79748b
Joel Mahoney <joelmahoney@gmail.com>
2014-10-01 18:03:39 -0700

    Refactor date helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,36 @@
 module ApplicationHelper
 
 	##### Devise mappings, from http://natashatherobot.com/devise-rails-sign-in/
-	
+
 	def resource_name
     :user
   end
- 
+
   def resource
     @resource ||= User.new
   end
- 
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
 
 	#####
+
+	def last_year_helper
+		if Date.today.month > 9
+			Date.today.year
+		else
+			Date.today.years_ago(1).year
+		end
+	end
+
+	def next_year_helper
+		if Date.today.month > 9
+			Date.today.years_since(1).year
+		else
+			Date.today.year
+		end
+	end
 
 	def timeline_percent(today)
 		if today >= Date.new(today.year, 10, 01)
