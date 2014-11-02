d645de2fa4bb7be5fb7e19b4814e44c6971fc1f7
Joel Mahoney <joelmahoney@gmail.com>
2014-11-02 11:15:16 -0800

    Eager load preferences on preference category

--- app/models/preference_category.rb
+++ app/models/preference_category.rb
@@ -1,22 +1,22 @@
 class PreferenceCategory < ActiveRecord::Base
 	include RankedModel
 	ranks :sort_order
-	
+
 	has_many :preferences
 
   attr_accessible :name, :description, :sort_order, :qualitative_criteria, :include_in_special_needs_dialog_box, :include_in_preferences_panel, :select_type, :glyph_id, :glyph_class
 
-   
+
   scope :special_needs, where(include_in_special_needs_dialog_box: true)
   scope :preference_panel, where(include_in_preferences_panel: true)
 
-  def self.grade_level_categories(gl)
-  	grade_level = gl.try(:downcase).try(:strip)
+  def self.grade_level_categories(grade)
+  	grade_level = grade.try(:downcase).try(:strip)
     PreferenceCategory.includes(:preferences).where("preferences.grade_#{grade_level} = ?", true).order("preference_categories.sort_order")
   end
 
-  def grade_level_preferences(gl)
-  	grade_level = gl.try(:downcase).try(:strip)
-  	self.preferences.where("grade_#{grade_level} = ?", true).rank(:sort_order)
+  def grade_level_preferences(grade)
+  	grade_level = grade.try(:downcase).try(:strip)
+  	Preference.includes(:preference_category).where("preference_category_id = ? AND preferences.grade_#{grade_level} = ?", self.id, true).rank(:sort_order)
   end
 end
