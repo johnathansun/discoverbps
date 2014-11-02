4660460fcec610bdfe761ae121980a395d18b234
Joel Mahoney <joelmahoney@gmail.com>
2014-11-02 13:21:15 -0800

    Consistent sort order across pages

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -8,10 +8,24 @@ class SchoolsController < ApplicationController
     else
       current_student.update_column(:step, 2) if current_student.step < 2
 
-      @home_schools = current_student.home_schools
-      @zone_schools = current_student.zone_schools
-      @ell_schools = current_student.ell_schools
-      @sped_schools = current_student.sped_schools
+      # rank the list if it has already been sorted
+      if current_student.student_schools.collect {|x| x.ranked}.any?
+        home_schools = current_student.home_schools.rank(:sort_order)
+      #show the stars first if the list had been starred
+      elsif current_student.starred_schools.present?
+        home_schools = current_student.starred_schools.all
+        current_student.home_schools.rank(:sort_order).all.each do |school|
+          home_schools << school unless home_schools.include?(school)
+        end
+      # match the default sort order on the lean page (distance)
+      else
+        home_schools = current_student.home_schools.order(:distance)
+      end
+
+      @home_schools = home_schools
+      @zone_schools = current_student.zone_schools.order(:distance)
+      @ell_schools = current_student.ell_schools.order(:distance)
+      @sped_schools = current_student.sped_schools.order(:distance)
 
       if @home_schools.blank?
         flash[:alert] = 'There were no schools that matched your search. Please try again.'
@@ -31,14 +45,24 @@ class SchoolsController < ApplicationController
     else
       current_student.update_column(:step, 3) if current_student.step < 3
 
-      home_schools = current_student.starred_schools.all
-      current_student.home_schools.rank(:sort_order).all.each do |school|
-        home_schools << school unless home_schools.include?(school)
+      # rank the list if it has already been sorted
+      if current_student.student_schools.collect {|x| x.ranked}.any?
+        home_schools = current_student.home_schools.rank(:sort_order)
+      #show the stars first if the list had been starred
+      elsif current_student.starred_schools.present?
+        home_schools = current_student.starred_schools.all
+        current_student.home_schools.rank(:sort_order).all.each do |school|
+          home_schools << school unless home_schools.include?(school)
+        end
+      # match the default sort order on the lean page (distance)
+      else
+        home_schools = current_student.home_schools.order(:distance)
       end
+
       @home_schools = home_schools
-      @zone_schools = current_student.zone_schools.rank(:sort_order)
-      @ell_schools = current_student.ell_schools.rank(:sort_order)
-      @sped_schools = current_student.sped_schools.rank(:sort_order)
+      @zone_schools = current_student.zone_schools.order(:distance)
+      @ell_schools = current_student.ell_schools.order(:distance)
+      @sped_schools = current_student.sped_schools.order(:distance)
 
       @matching_school_ids = current_user_students.collect {|x| x.student_schools.collect {|y| y.bps_id}}.inject(:&)
 
@@ -60,10 +84,24 @@ class SchoolsController < ApplicationController
 
       current_student.update_column(:step, 4) if current_student.step < 4
 
-      @home_schools = current_student.home_schools
-      @zone_schools = current_student.zone_schools
-      @ell_schools = current_student.ell_schools
-      @sped_schools = current_student.sped_schools
+      # rank the list if it has already been sorted
+      if current_student.student_schools.collect {|x| x.ranked}.any?
+        home_schools = current_student.home_schools.rank(:sort_order)
+      #show the stars first if the list had been starred
+      elsif current_student.starred_schools.present?
+        home_schools = current_student.starred_schools.all
+        current_student.home_schools.rank(:sort_order).all.each do |school|
+          home_schools << school unless home_schools.include?(school)
+        end
+      # match the default sort order on the lean page (distance)
+      else
+        home_schools = current_student.home_schools.order(:distance)
+      end
+
+      @home_schools = home_schools
+      @zone_schools = current_student.zone_schools.order(:distance)
+      @ell_schools = current_student.ell_schools.order(:distance)
+      @sped_schools = current_student.sped_schools.order(:distance)
 
       if @home_schools.blank?
         flash[:alert] = 'There were no schools that matched your search. Please try again.'
@@ -90,9 +128,6 @@ class SchoolsController < ApplicationController
         if student_school.present?
           student_school.update_attributes(sort_order_position: i, ranked: true)
         end
-      end
-      respond_to do |format|
-        format.js { render template: "schools/actions/sort" }
       end
     end
   end
