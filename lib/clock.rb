10c85b7509d1058142031c8af883806c3eb26221
Joel Mahoney <joelmahoney@gmail.com>
2014-11-10 09:51:26 -0800

    Update clock file times

--- lib/clock.rb
+++ lib/clock.rb
@@ -9,19 +9,19 @@ handler do |job|
 end
 
 # 9 UTC = 3am EST and 12am PST
-every(1.day, 'Update school basic info', at: '09:00') 		    { SchoolData.delay.update_basic_info! }
-every(1.day, 'Update school awards', at: '09:00') 				    { SchoolData.delay.update_awards! }
-every(1.day, 'Update school descriptions', at: '09:00')       { SchoolData.delay.update_descriptions! }
-every(1.day, 'Update school facilities', at: '09:00') 		    { SchoolData.delay.update_facilities! }
-every(1.day, 'Update school grades', at: '09:00') 				    { SchoolData.delay.update_grades! }
-every(1.day, 'Update school hours', at: '09:00') 					    { SchoolData.delay.update_hours! }
-every(1.day, 'Update school languages', at: '09:00')	 		    { SchoolData.delay.update_languages! }
-every(1.day, 'Update school partners', at: '09:00')           { SchoolData.delay.update_partners! }
-every(1.day, 'Update school photos', at: '09:00') 				    { SchoolData.delay.update_photos! }
-every(1.day, 'Update school preview dates', at: '09:00') 		  { SchoolData.delay.update_preview_dates! }
-every(1.day, 'Update school programs', at: '09:00') 				  { SchoolData.delay.update_programs! }
-every(1.day, 'Update school sports', at: '09:00') 				    { SchoolData.delay.update_sports! }
-every(1.day, 'Update school student support', at: '09:00')    { SchoolData.delay.update_student_support! }
-every(1.day, 'Update school surround care', at: '09:00')      { SchoolData.delay.update_surround_care! }
+every(1.day, 'Update school basic info', :at => '6:00', :tz => 'America/New_York') 		    { SchoolData.delay.update_basic_info! }
+every(1.day, 'Update school awards', :at => '6:01', :tz => 'America/New_York') 				    { SchoolData.delay.update_awards! }
+every(1.day, 'Update school descriptions', :at => '6:02', :tz => 'America/New_York')      { SchoolData.delay.update_descriptions! }
+every(1.day, 'Update school facilities', :at => '6:03', :tz => 'America/New_York') 		    { SchoolData.delay.update_facilities! }
+every(1.day, 'Update school grades', :at => '6:04', :tz => 'America/New_York') 				    { SchoolData.delay.update_grades! }
+every(1.day, 'Update school hours', :at => '6:05', :tz => 'America/New_York') 					  { SchoolData.delay.update_hours! }
+every(1.day, 'Update school languages', :at => '6:06', :tz => 'America/New_York')	 		    { SchoolData.delay.update_languages! }
+every(1.day, 'Update school partners', :at => '6:07', :tz => 'America/New_York')          { SchoolData.delay.update_partners! }
+every(1.day, 'Update school photos', :at => '6:08', :tz => 'America/New_York') 				    { SchoolData.delay.update_photos! }
+every(1.day, 'Update school preview dates', :at => '6:09', :tz => 'America/New_York') 		{ SchoolData.delay.update_preview_dates! }
+every(1.day, 'Update school programs', :at => '6:10', :tz => 'America/New_York') 				  { SchoolData.delay.update_programs! }
+every(1.day, 'Update school sports', :at => '6:11', :tz => 'America/New_York') 				    { SchoolData.delay.update_sports! }
+every(1.day, 'Update school student support', :at => '6:12', :tz => 'America/New_York')   { SchoolData.delay.update_student_support! }
+every(1.day, 'Update school surround care', :at => '6:13', :tz => 'America/New_York')     { SchoolData.delay.update_surround_care! }
 
-every(1.hour, 'Store searches json') { StoredSearch.first_or_create.update_attributes(json: Student.order(:last_name).to_json(only: [ :grade_level, :latitude, :longitude, :zipcode, :ell_language, :sped_needs, :awc_invitation, :preferences_count  ], methods: :created_at_date)) }
+every(6.hours, 'Store searches json') { StoredSearch.first_or_create.update_attributes(json: Student.order(:last_name).to_json(only: [ :grade_level, :latitude, :longitude, :zipcode, :ell_language, :sped_needs, :awc_invitation, :preferences_count  ], methods: :created_at_date)) }
