5fcd0ec94da08c9a2ca98e9015ac770e3d2d5a45
Joel Mahoney <joelmahoney@gmail.com>
2015-09-30 00:07:25 -0700

    Work on user registration bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,24 +3,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     build_resource(sign_up_params)
-
-    if resource.save
-      yield resource if block_given?
-      if resource.active_for_authentication?
-        # set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        logger.info "****************** resource active_for_authentication?"
-        return render :json => {:success => true}
+    
+    respond_to do |format|
+    
+      if resource.save!
+        yield resource if block_given?
+        if resource.active_for_authentication?
+          # set_flash_message :notice, :signed_up if is_flashing_format?
+          sign_up(resource_name, resource)
+          logger.info "****************** resource active_for_authentication?"
+          format.json { render :json => {:success => true} }
+          format.html { redirect_to root_url }
+        else
+          # set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+          expire_data_after_sign_in!
+          logger.info "****************** resource not active_for_authentication"
+  	      format.json { render :json => {:success => true} }
+          format.html { redirect_to root_url}
+        end
       else
-        # set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        logger.info "****************** resource not active_for_authentication"
-	      return render :json => {:success => true}
+        clean_up_passwords resource
+        logger.info "****************** resource not saved"
+  	    format.json { render :json => {:success => false, :errors => ["Signup failed."]} }
+        format.html { redirect_to root_url }
       end
-    else
-      clean_up_passwords resource
-      logger.info "****************** resource not saved"
-	    return render :json => {:success => false, :errors => ["Signup failed."]}
     end
   end
 end
