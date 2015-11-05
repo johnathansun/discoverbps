9a26e9f3691a5dbb3bbddbcbf5f64f585c5088e7
Joel Mahoney <joelmahoney@gmail.com>
2015-11-04 18:30:52 -0800

    Tweak error email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ActionMailer::Base
 
   def api_error(endpoint, bps_id)
     @endpoint = endpoint
-    @bps_id = bps_id
+    @school = School.where(bps_id: bps_id).first
 
     mail(to: "joelmahoney@gmail.com", from: "errors@discoverbps.org", subject: "API Error")
   end
