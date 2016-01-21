13d55256d8be81f7de54f2b2e0b5ca4839876c76
Joel Mahoney <joelmahoney@gmail.com>
2016-01-21 07:40:54 -0800

    Fix new_student_preference_path link

--- app/controllers/student_ell_preferences_controller.rb
+++ app/controllers/student_ell_preferences_controller.rb
@@ -19,8 +19,8 @@ class StudentEllPreferencesController < ApplicationController
           current_student.set_ell_schools
         end
 
-        format.html { redirect_to new_student_sped_preferences_path }
         format.js { render template: "student_sped_preferences/new" }
+        format.html { redirect_to new_student_sped_preference_path }
       else
         format.js { render template: "student_ell_preferences/new" }
         flash[:alert] = 'There were problems with your search. Please complete the required fields and try again.'
