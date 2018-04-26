864a8959a2cb32ec8d190cd2e934e6294a472576
Nikhil Chikorde <nikhilc.developer@gmail.com>
2018-04-26 13:22:00 -0400

    timeout

--- lib/webservice.rb
+++ lib/webservice.rb
@@ -307,7 +307,7 @@ module Webservice
 	end
 
 	def self.postWithHeader(headerKey, endpoint, payload)
-		Faraday.new(url: "#{endpoint}", request: {timeout: 500}).post do |req|
+		Faraday.new(url: "#{endpoint}", request: {timeout: 60}).post do |req|
 		  req.headers["Content-Type"] = "application/json"
 		  req.headers["BpsToken"] = headerKey
 			req.body = payload.to_json
