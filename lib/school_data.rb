ae677a4e6262d01b8eaa99d2889ab792514caa1d
Nikhil Chikorde <nikhilc.developer@gmail.com>
2018-07-28 10:31:31 -0400

    commented because WEBSERVICE_URL is not working

--- lib/school_data.rb
+++ lib/school_data.rb
@@ -5,12 +5,12 @@ module SchoolData
   def self.update_basic_info!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_basic_info(school.bps_id)
-      if response.present?
-        school.update_attributes(name: response[:schname_23], latitude: response[:Latitude], longitude: response[:Longitude], api_basic_info: response, last_sync: Time.now, last_sync_basic_info: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_basic_info(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(name: response[:schname_23], latitude: response[:Latitude], longitude: response[:Longitude], api_basic_info: response, last_sync: Time.now, last_sync_basic_info: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE AWARDS #####
@@ -18,12 +18,12 @@ module SchoolData
   def self.update_awards!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_awards(school.bps_id)
-      if response.present?
-        school.update_attributes(api_awards: response, last_sync: Time.now, last_sync_awards: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_awards(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_awards: response, last_sync: Time.now, last_sync_awards: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE DESCRIPTIONS #####
@@ -31,12 +31,12 @@ module SchoolData
   def self.update_descriptions!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_description(school.bps_id)
-      if response.present?
-        school.update_attributes(api_description: response, last_sync: Time.now, last_sync_descriptions: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_description(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_description: response, last_sync: Time.now, last_sync_descriptions: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE FACILITIES #####
@@ -44,12 +44,12 @@ module SchoolData
   def self.update_facilities!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_facilities(school.bps_id)
-      if response.present?
-        school.update_attributes(api_facilities: response, last_sync: Time.now, last_sync_facilities: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_facilities(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_facilities: response, last_sync: Time.now, last_sync_facilities: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE GRADES #####
@@ -57,12 +57,12 @@ module SchoolData
   def self.update_grades!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_grades(school.bps_id)
-      if response.present?
-        school.update_attributes(api_grades: response, last_sync: Time.now, last_sync_grades: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_grades(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_grades: response, last_sync: Time.now, last_sync_grades: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE HOURS #####
@@ -70,12 +70,12 @@ module SchoolData
   def self.update_hours!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_hours(school.bps_id)
-      if response.present?
-        school.update_attributes(api_hours: response, last_sync: Time.now, last_sync_hours: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_hours(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_hours: response, last_sync: Time.now, last_sync_hours: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE LANGUAGES #####
@@ -83,12 +83,12 @@ module SchoolData
   def self.update_languages!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_languages(school.bps_id)
-      if response.present?
-        school.update_attributes(api_languages: response, last_sync: Time.now, last_sync_languages: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_languages(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_languages: response, last_sync: Time.now, last_sync_languages: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE PARTNERS #####
@@ -96,12 +96,12 @@ module SchoolData
   def self.update_partners!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_partners(school.bps_id)
-      if response.present?
-        school.update_attributes(api_partners: response, last_sync_partners: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_partners(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_partners: response, last_sync_partners: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE PHOTOS #####
@@ -109,12 +109,12 @@ module SchoolData
   def self.update_photos!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_photos(school.bps_id)
-      if response.present?
-        school.update_attributes(api_photos: response, last_sync: Time.now, last_sync_photos: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_photos(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_photos: response, last_sync: Time.now, last_sync_photos: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE PREVIEW DATES #####
@@ -122,12 +122,12 @@ module SchoolData
   def self.update_preview_dates!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_preview_dates(school.bps_id)
-      if response.present?
-        school.update_attributes(api_preview_dates: response, last_sync: Time.now, last_sync_preview_dates: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_preview_dates(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_preview_dates: response, last_sync: Time.now, last_sync_preview_dates: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE PROGRAMS #####
@@ -135,12 +135,12 @@ module SchoolData
   def self.update_programs!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_programs(school.bps_id)
-      if response.present?
-        school.update_attributes(api_programs: response, last_sync: Time.now, last_sync_programs: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_programs(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_programs: response, last_sync: Time.now, last_sync_programs: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE SPORTS #####
@@ -148,12 +148,12 @@ module SchoolData
   def self.update_sports!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_sports(school.bps_id)
-      if response.present?
-        school.update_attributes(api_sports: response, last_sync: Time.now, last_sync_sports: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_sports(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_sports: response, last_sync: Time.now, last_sync_sports: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE STUDENT SUPPORT #####
@@ -161,12 +161,12 @@ module SchoolData
   def self.update_student_support!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_student_support(school.bps_id)
-      if response.present?
-        school.update_attributes(api_student_support: response, last_sync: Time.now, last_sync_student_support: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_student_support(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_student_support: response, last_sync: Time.now, last_sync_student_support: Time.now)
+    #   end
+    # end
   end
 
   ##### UPDATE OTHER PROGRAMS #####
@@ -174,12 +174,12 @@ module SchoolData
   def self.update_surround_care!(school_id=nil)
     schools = self.find_schools(school_id)
 
-    schools.each do |school|
-      response = Webservice.get_surround_care(school.bps_id)
-      if response.present?
-        school.update_attributes(api_surround_care: response, last_sync: Time.now, last_sync_surround_care: Time.now)
-      end
-    end
+    # schools.each do |school|
+    #   response = Webservice.get_surround_care(school.bps_id)
+    #   if response.present?
+    #     school.update_attributes(api_surround_care: response, last_sync: Time.now, last_sync_surround_care: Time.now)
+    #   end
+    # end
   end
 
 
