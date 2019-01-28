9228aa24fa58b344b3bc7f59ad6e0a490fd734fe
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-01-28 17:22:28 -0500

    Updated code changes

--- app/models/student_school.rb
+++ app/models/student_school.rb
@@ -11,35 +11,33 @@ class StudentSchool < ActiveRecord::Base
                   :tier_explanation, :school_dese_accountability
 
   def self.create_from_api_response(student, school, school_hash, school_list_type)
-
     Rails.logger.info "******************* #{school_hash}"
-
     self.create!(student_id: student.id,
-      school_id: school.id,
-      school_name: school_hash.try(:[],[:SchoolName]),
-      school_type: school_list_type,
-      bps_id: (school_list_type == "ell" ? school_hash.try(:[],[:SchoolId]) : school_hash.try(:[],[:SchoolLocalId])),
-      tier: school_hash.try(:[],[:Tier]),
-      eligibility: school_hash.try(:[],[:SchoolEligibility]),
-      walk_zone_eligibility: school_hash.try(:[],[:AssignmentWalkEligibilityStatus]),
-      transportation_eligibility: school_hash.try(:[],[:TransportationEligibility]),
-      distance: school_hash.try(:[],[:StraightLineDistance]),
-      exam_school: ((school_hash.try(:[], school_hash[:IsExamSchool] && school_hash[:IsExamSchool] == '0') ? false : true)),
-      sped_cluster: school_hash.try(:[],[:SPEDCluster]),
-      sped_description: school_hash.try(:[],[:Program]),
-      ell_cluster: school_hash.try(:[],[:EllCluster]),
-      ell_description: school_hash.try(:[],[:ProgramDescription]),
-      program_code: (school_list_type == "ell" ? school_hash.try(:[],[:ProgramCode]) : school_hash.try(:[],[:ProgramId])),
-      program_code_description: school_hash.try(:[],[:ProgramCodeDescription]),
-      call_id: school_hash[:CallId],
-      special_admissions: school_hash.try(:[],[:IsSpecAdmissions]),
-      walk_distance: school_hash.try(:[],[:WalkDistance]),
-      dese_tier: school_hash.try(:[],[:DeseTier]),
-      #TODO: was not included before. Verify once
-      # sort_order: school_hash[:SortOrder]
-      choice_rank: school_hash.try(:[],[:ChoiceRank]),
-      tier_explanation: school_hash.try(:[], [:TierExplanation]),
-      school_dese_accountability: school_hash.try(:[],[:SchoolDESEAccountability])
+     school_id: school.id,
+     school_name: school_hash[:SchoolName],
+     school_type: school_list_type,
+     bps_id: (school_list_type == "ell" ? school_hash[:SchoolId] : school_hash[:SchoolLocalId]),
+     tier: school_hash[:Tier],
+     eligibility: school_hash[:SchoolEligibility],
+     walk_zone_eligibility: school_hash[:AssignmentWalkEligibilityStatus],
+     transportation_eligibility: school_hash[:TransportationEligibility],
+     distance: school_hash[:StraightLineDistance],
+     exam_school: (school_hash[:IsExamSchool] == "0" ? false : true),
+     sped_cluster: school_hash[:SPEDCluster],
+     sped_description: school_hash[:Program],
+     ell_cluster: school_hash[:EllCluster],
+     ell_description: school_hash[:ProgramDescription],
+     program_code: (school_list_type == "ell" ? school_hash[:ProgramCode] : school_hash[:ProgramId]),
+     program_code_description: school_hash[:ProgramCodeDescription],
+     call_id: school_hash[:CallId],
+     special_admissions: school_hash[:IsSpecAdmissions],
+     walk_distance: school_hash[:WalkDistance],
+     dese_tier: school_hash[:DeseTier],
+     #TODO: was not included before. Verify once
+     # sort_order: school_hash[:SortOrder]
+     choice_rank: school_hash[:ChoiceRank],
+     tier_explanation: school_hash[:TierExplanation],
+     school_dese_accountability: school_hash[:SchoolDESEAccountability]
     )
   end
 end
